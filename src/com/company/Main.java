720f0f5899294ab1e19a1e11736117813f4d5f5d
MrSerbes <emreserbes8391@gmail.com>
2021-10-08 17:27:36 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static com.company.Player.*;
+import static com.company.Player.Choices.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(folksDev);
+        System.out.println("Welcome to Paper-Rock-Scissors game");
+        System.out.print("Please enter your name : ");
+
+        Scanner sc = new Scanner(System.in);
+
+        Player player1=new Player("Computer");
+        Player player2=new Player(sc.nextLine());
+
+        System.out.print("How many times do you want to play the game? : ");
+        int times=0,i=0;
+        times=sc.nextInt();
+
+        while (i<times){
+            System.out.print("Enter your move : ");
+            player1.setChoice(player1.playRandom());
+            player2.setChoice(player2.getPlayerMove());
+            System.out.println("\n"+player1.getName()+" played : "+player1.getChoise()+" ------- "+player2.getName()+" played : "+player2.getChoise());
+
+            if (player1.getChoise() == player2.getChoise())
+            {
+                System.out.println("It is a tie");
+            } else {
+                switch (player1.getChoise()) {
+                    case ROCK:
+                        if (player2.getChoise() == SCISSORS){
+                            System.out.println(player1.getName() + " wins!");
+                            player1.earnPoints();
+                        }
+                        else{
+                            System.out.println(player2.getName() + " wins!");
+                            player2.earnPoints();
+                        }
+                        break;
+                    case SCISSORS:
+                        if (player2.getChoise() == PAPER){
+                            System.out.println(player1.getName() + " wins!");
+                            player1.earnPoints();
+                        }
+                        else{
+                            System.out.println(player2.getName() + " wins!");
+                            player2.earnPoints();
+                        }
+                        break;
+                    case PAPER:
+                        if (player2.getChoise() == ROCK){
+                            System.out.println(player1.getName() + " wins!");
+                            player1.earnPoints();
+                        }
+                        else{
+                            System.out.println(player2.getName() + " wins!");
+                            player2.earnPoints();
+                        }
+                        break;
+                }
+            }
+            i++;
+            System.out.println(i+". result is : "+player1.getName()+" = "+player1.getScore()+" ------- "+player2.getName()+" = "+player2.getScore()+"\n");
+        }
+        System.out.println("Game result is : "+player1.getName()+" = "+player1.getScore()+" ------- "+player2.getName()+" = "+player2.getScore());
+        if(player1.getScore()==player2.getScore()){
+            System.out.println("The game ended in a draw");
+        }else{
+            System.out.println(((player1.getScore() > player2.getScore()) ? player1.getName() :player2.getName())+" win the game.");
+        }
+
+    }
+
+
+    private static final String folksDev =
+            """
+                    ███████╗░█████╗░██╗░░░░░██╗░░██╗░██████╗██████╗░███████╗██╗░░░██╗
+                    ██╔════╝██╔══██╗██║░░░░░██║░██╔╝██╔════╝██╔══██╗██╔════╝██║░░░██║
+                    █████╗░░██║░░██║██║░░░░░█████═╝░╚█████╗░██║░░██║█████╗░░╚██╗░██╔╝
+                    ██╔══╝░░██║░░██║██║░░░░░██╔═██╗░░╚═══██╗██║░░██║██╔══╝░░░╚████╔╝░
+                    ██║░░░░░╚█████╔╝███████╗██║░╚██╗██████╔╝██████╔╝███████╗░░╚██╔╝░░
+                    ╚═╝░░░░░░╚════╝░╚══════╝╚═╝░░╚═╝╚═════╝░╚═════╝░╚══════╝░░░╚═╝░░░""";
+}
---
+++ src/com/company/Player.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class Player {
+    private int score=0;
+    private String name;
+    public enum Choices {
+        PAPER,
+        ROCK,
+        SCISSORS
+    }
+    private Choices choice;
+
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public void write(){
+        System.out.println(choice.name());
+    }
+
+    public void earnPoints(){
+        this.score++;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setChoice(Choices choice){
+        this.choice=choice;
+    }
+
+    public Choices getChoise(){
+        return this.choice;
+    }
+
+    public Choices getPlayerMove(){
+        Scanner scan = new Scanner(System.in);
+        while (true){
+            String choice = scan.nextLine();
+            String playerChoice = choice.toUpperCase();
+            if(playerChoice.equals("ROCK") || playerChoice.equals("PAPER") || playerChoice.equals("SCISSORS")){
+                return Choices.valueOf(playerChoice);
+            }else{
+                System.out.println("This is not Valid Move, Try again! Paper-Rock-Scissors");
+            }
+        }
+    }
+
+    public Choices playRandom(){
+        return Choices.values()[ new SecureRandom().nextInt(Choices.values().length)];
+    }
+
+}
